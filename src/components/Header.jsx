83c457243473b0868815342f8acee69ebdce004a
liasovIhsan04 <liasovihsan1@gmail.com>
2024-02-08 20:55:22 +0600

    ihsan

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,45 +1,19 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { BiBell } from "react-icons/bi";
 import logoalma from "../img/logo-alma.svg";
 import "../style/css/App.css";
 import "../style/css/media.css";
 import "../style/css/main.css";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { auth } from "../Redux/reduser/auth";
 
-const Header = ({ users }) => {
+const Header = () => {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const token = localStorage.getItem("token")
-    ? localStorage.getItem("token")
-    : false;
-
-  const data = useSelector((state) => state.users);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        await dispatch(auth());
-      } catch (error) {
-        console.error("Ошибка инициализации user-info:", error);
-      }
-    };
-    fetchData();
-  }, [dispatch]);
 
   return (
     <div className="nav relative">
       <div className="container nav_content">
         <span></span>
-        {/* {token ? (
-          <h2 className="first_last_name_header" style={{ color: "#fff" }}>
-            Привет, {data && data.user.first_name} {data && data.user.last_name}
-            !
-          </h2>
-        ) : ( */}
         <img className="alma_header_img" src={logoalma} alt="" />
-        {/* )} */}
         <BiBell
           className="fi"
           color="#191919"
--- src/pages/ToHelpPage.jsx
+++ src/pages/ToHelpPage.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "../style/css/main.css";
 import { useNavigate } from "react-router";
-import { HiArrowLongLeft } from "react-icons/hi2";
-import { FiPhone } from "react-icons/fi";
-import { LiaTelegramPlane } from "react-icons/lia";
-import { BsWhatsapp } from "react-icons/bs";
-import ihsan from "../../src/img/support-bek.svg";
-import { IoMailOutline } from "react-icons/io5";
-
+import ihsan from "../../src/img/logo-alma.svg";
+import more from "../img/more.svg";
+import phone from "../img/phone_alma.svg";
+import whatsap from "../img/whatsap_alma.svg";
+import telegram from "../img/telegram_alma.svg";
+import instagram from "../img/instagram_alma.svg";
+import tikTok from "../img/Tik Tok_alma.svg";
+import youtube from "../img/youtube_alma.svg";
 const ToHelpPage = () => {
   const navigate = useNavigate();
 
@@ -16,14 +17,14 @@ const ToHelpPage = () => {
       <div className="to_help_page">
         <div className="nav">
           <div className="container d-flex justify-content-between align-items-center ">
-            <HiArrowLongLeft
-              className="fi"
-              style={{ cursor: "pointer" }}
+            <img
+              className="more_img"
               onClick={() => navigate(-1)}
-              size={20}
+              src={more}
+              alt=""
             />
-            <p className="header_name">Поддержка</p>
-            <span></span>
+            <p className="header_name">Настройки</p>
+            <div></div>
           </div>
         </div>
         <div className="container">
@@ -34,52 +35,55 @@ const ToHelpPage = () => {
             <div className="support_text">
               <h3>Возникли вопросы?</h3>
               <p className="title_h6">
-                Наша поддержка проконсультирует Вас по любым возникшим вопросам.
+                Наша поддержка проконсультирует Вас по любым возникшим вопросам
               </p>
+              <div className="to_contact">
+                <a href="tel:+996705000003">
+                  <div className="contact_box">
+                    <img src={phone} alt="" />
+                  </div>
+                  <div></div>
+                </a>
+                <a href="https://t.me/+996705000003" target="_blank">
+                  <div className="contact_box">
+                    <img src={whatsap} alt="" />
+                  </div>
+                  <div></div>
+                </a>
+                <a href="https://wa.me/+996705000003" target="_blank">
+                  <div className="contact_box">
+                    <img src={telegram} alt="" />
+                  </div>
+                  <div></div>
+                </a>
+              </div>
             </div>
-            <div className="to_contact">
-              <a href="tel:+996705000003">
-                <div className="contact_box">
-                  <FiPhone size={23} />
-                </div>
-                <div>
-                  <p className="title_h6" style={{ marginTop: "8px" }}>
-                    Позвонить
-                  </p>
-                </div>
-              </a>
-              <a href="https://t.me/+996705000003" target="_blank">
-                <div className="contact_box">
-                  <LiaTelegramPlane size={23} />
-                </div>
-                <div>
-                  <p className="title_h6" style={{ marginTop: "8px" }}>
-                    Telegram
-                  </p>
-                </div>
-              </a>
-              <a href="https://wa.me/+996705000003" target="_blank">
-                <div className="contact_box">
-                  <BsWhatsapp size={23} />
-                </div>
-                <div>
-                  <p className="title_h6" style={{ marginTop: "8px" }}>
-                    Whatsapp
-                  </p>
-                </div>
-              </a>
-              <a href="#" target="_blank">
-                <div className="contact_box">
-                  <a href="mailto:aaammuu@bk.ru" className="fi">
-                    <IoMailOutline className="" size={23} />
-                  </a>
-                </div>
-                <div>
-                  <p className="title_h6" style={{ marginTop: "8px" }}>
-                    Написать
-                  </p>
-                </div>
-              </a>
+            <div className="support_text sup-top">
+              <h3>Мы в социальных сетях</h3>
+              <p className="title_h6">
+                Подписывайтесь на наши социальные сети и будьте в курсе всех
+                новостей
+              </p>
+              <div className="to_contact">
+                <a href="tel:+996705000003">
+                  <div className="contact_box">
+                    <img src={instagram} alt="" />
+                  </div>
+                  <div></div>
+                </a>
+                <a href="https://t.me/+996705000003" target="_blank">
+                  <div className="contact_box">
+                    <img src={tikTok} alt="" />
+                  </div>
+                  <div></div>
+                </a>
+                <a href="https://wa.me/+996705000003" target="_blank">
+                  <div className="contact_box">
+                    <img src={youtube} alt="" />
+                  </div>
+                  <div></div>
+                </a>
+              </div>
             </div>
           </div>
         </div>
